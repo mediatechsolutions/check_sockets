c50886728e4c4b97a203cd8b45fc9480ed77af79
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2018-08-02 11:00:38 +0000

    Allow to run it in parallel and with cache

--- check_sockets_on_kubernetes.py
+++ check_sockets_on_kubernetes.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 import argparse
 import json
+import os
 import re
 import sys
 import subprocess
+import logging
+import time
+from multiprocessing import Pool
+
 
 nagios_output_state = {
     'OK': 0,
@@ -12,10 +17,42 @@ nagios_output_state = {
     'UNKNOWN': 3,
 }
 
+logger = logging.getLogger()
+ERROR = logging.ERROR
+WARNING = logging.WARNING
+INFO = logging.INFO
+DETAIL = logging.INFO - 1
+DEBUG = logging.DEBUG
+VERBOSITIES = [ERROR, WARNING, INFO, DETAIL, DEBUG]
+
+
+def configure_logging(verbosity):
+    level = VERBOSITIES[min(int(verbosity), len(VERBOSITIES) -1)]
+    formatter = logging.Formatter('** %(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(level)
+
 
 def execute_command(cmd):
     p = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return p.communicate()
+
+
+def parallel_k8s_run(args):
+    kubeconfig, cmd, podname = args
+    logger.debug("parallel_k8s_run(%s, %s)", cmd, podname)
+    command = "kubectl --kubeconfig %s exec %s -- %s" % (kubeconfig, podname, cmd)
+    logger.debug("Executing command %s", command)
+    output, errors = execute_command(command)
+    return (
+        podname,
+        dict(
+            stdout=output,
+            stderr=errors,
+        )
+    )
 
 
 class Kubernetes(object):
@@ -24,21 +61,40 @@ class Kubernetes(object):
 
     def get_all_pod_names(self):
         cmd = "kubectl --kubeconfig %s get pods --field-selector=status.phase=Running -o custom-columns=:metadata.name" % self.kubeconfig
+        logger.debug("Executing command %s", cmd)
         output, errors = execute_command(cmd)
-        return output.split('\n')
+        return [x.strip() for x in output.split('\n') if x.strip()]
 
-    def execute_command_on_all_pods(self, cmd):
+    def execute_command_on_pod(self, cmd, podname):
+        logger.debug("execute_command_on_pod(%s, %s)", cmd, podname)
+        command = "kubectl --kubeconfig %s exec %s -- %s" % (self.kubeconfig, podname, cmd)
+        logger.debug("Executing command %s", command)
+        output, errors = execute_command(command)
+        return dict(
+            stdout=output,
+            stderr=errors,
+        )
+
+    def execute_command_on_pod_tuple(self, t):
+        return self.execute_command_on_pod(t[0], t[1])
+
+    def execute_command_on_all_pods(self, cmd, fast=False):
         result = dict()
 
-        for pod in self.get_all_pod_names():
-            if pod == '' or pod == None:
-                continue
-            command = "kubectl --kubeconfig %s exec -ti %s -- %s" % (self.kubeconfig, pod, cmd)
-            output, errors = execute_command(command)
-            result[pod] = dict(
-                stdout=output,
-                stderr=errors,
-            )
+        podnames = self.get_all_pod_names()
+        logger.debug("%s pods found", len(podnames))
+        logger.debug("pods: %s", podnames)
+
+        if fast:
+            logger.debug("Fast mode enabled")
+            pool = Pool()
+            raw = pool.map(parallel_k8s_run, [(self.kubeconfig, cmd, pod) for pod in podnames])
+            logger.debug("result generated")
+            for r in raw:
+                result[r[0]] = r[1]
+        else:
+            for pod in podnames:
+                result[pod] = self.execute_command_on_pod(cmd, pod)
         return result
 
 
@@ -57,11 +113,12 @@ class Checker(object):
         self.filter = filter
 
         self.check_status = 'OK'
+        self.fast = args.fast
 
     def __add_performance_data(self, stats):
         for container, stats in stats.items():
             key_name = '%s.%s' % (container, stats['protocol'])
-            self.perf_data.insert(0, ('%s.open=%s;%s;%s;;' % (key_name, stats['open'], self.warning, self.critical)))
+            self.perf_data.insert(0, ('%s.open=%s;%s;%s;;\n' % (key_name, stats['open'], self.warning, self.critical)))
 #            self.perf_data.insert(0, ('%s.waiting_to_close=%s;;;;' % (key_name, stats['waiting_to_close'])))
 
     def __calculate_performance_data(self, output):
@@ -100,17 +157,15 @@ class Checker(object):
                 self.summary.append('WARNING %s open sockets %s > %s' % (container, stats['open'], self.warning))
 
     def check(self):
-        result = self.kubernetes.execute_command_on_all_pods('cat /proc/net/sockstat')
+        result = self.kubernetes.execute_command_on_all_pods('cat /proc/net/sockstat', self.fast)
         socket_data = self.__get_socket_data(result)
 
         #self.data.append(json.dumps(socket_data, indent=4, sort_keys=True))
 
         self.__calculate_status(socket_data)
         self.__add_performance_data(socket_data)
-        self.__nagios_output()
 
-
-    def __nagios_output(self):
+    def nagios_output(self):
         output = self.check_status
 
         if self.summary:
@@ -120,11 +175,36 @@ class Checker(object):
         if self.enable_performance_data:
             output += '\n\n|%s' % (' '.join(self.perf_data))
 
-        print(output)
+        return output
+
+    def nagios_exit(self):
+        print(self.nagios_output())
         sys.exit(nagios_output_state[self.check_status])
 
 
-if __name__ == "__main__":
+class Cache(object):
+    def __init__(self, default_content='Not executed', default_rc=3, cache_liveness=60):
+        self.filename = "/tmp/check_sockets.json"
+        self.default_content = default_content
+        self.default_rc = default_rc
+        self.cache_liveness = cache_liveness
+
+    def write(self, output, rc):
+        with open(self.filename, 'w+') as fd:
+            json.dump(dict(output=output, rc=rc), fd)
+
+    def read(self):
+        if (
+            not os.path.exists(self.filename)
+            or os.stat(self.filename).st_mtime + self.cache_liveness< time.time()
+        ):
+            return (self.default_content, self.default_rc)
+        with open(self.filename) as fd:
+            data = json.load(fd)
+        return (data['output'], data['rc'])
+           
+ 
+def get_args():
     parser = argparse.ArgumentParser(
         description='Return number of open sockets')
 
@@ -148,9 +228,59 @@ if __name__ == "__main__":
         default='25000',
         type=int,
     )
+    parser.add_argument(
+        '-f', '--fast',
+        action="store_true",
+        default=False,
+        help='Run in threads in order to increase speed.'
+    )
+    parser.add_argument(
+        '-d', '--delayed',
+        action="store_true",
+        default=False,
+        help='Returns the last value or empty and runs it in background for the next time'
+    )
+    parser.add_argument(
+        '--cache-liveness',
+        default=60 * 60,  # seconds
+        help='Seconds the cache file is valid. After that time it would be not considered.'
+    )
+    parser.add_argument(
+        '-v', '--verbose',
+        action='count',
+        dest='verbosity',
+        default=0,
+        help='Increase verbosity. Several increases even more.'
+    )
 
     args = parser.parse_args()
+    return args
+
+
+def main():
+    args = get_args()
+    configure_logging(args.verbosity)
 
     check = Checker(args)
-    check.check()
 
+    if args.delayed:
+        cache = Cache(cache_liveness=args.cache_liveness)
+        pid = os.fork() 
+        if pid > 0:
+            output, rc = cache.read()
+            print(output)
+            sys.exit(rc)
+            return
+        
+        check.check()
+        output = check.nagios_output()
+        rc = nagios_output_state[check.check_status]
+        cache.write(output, rc)
+        return
+
+    check.check()
+    check.nagios_exit()
+
+
+if __name__ == "__main__":
+    main()
